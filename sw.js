5b370e31f3b4b9293b05b287e715a678cb406817
Camilo Martinez <imagina3ddesign@gmail.com>
2026-06-27 06:17:13 -0500

    Update Azure Client ID to new App Registration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-bim-alcabama-v9';
+const CACHE_NAME = 'portal-bim-alcabama-v10';
 // Lista de archivos que queremos guardar en caché para que la app funcione offline.
 const urlsToCache = [
   'home.html', // Ruta relativa a la ubicación del Service Worker
