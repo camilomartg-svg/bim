bf34fb9f0970b4dfde3beee75514b7c913ce0586
Camilo Martinez <imagina3ddesign@gmail.com>
2026-06-29 17:11:39 -0500

    UI: Ajustes en empresas.html y adicion de favicon global

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-bim-alcabama-v18';
+const CACHE_NAME = 'portal-bim-alcabama-v19';
 // Lista de archivos que queremos guardar en caché para que la app funcione offline.
 const urlsToCache = [
   'home.html', // Ruta relativa a la ubicación del Service Worker
